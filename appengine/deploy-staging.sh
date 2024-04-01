a54de2ee21d954a9397452084dff471ee9d970b1
wongje11 <wongje11@gmail.com>
2024-04-01 18:33:13 +0200

    Comment out two other deployments

--- appengine/deploy-staging.sh
+++ appengine/deploy-staging.sh
@@ -9,8 +9,10 @@ set -x
 gcloud --quiet app deploy \
   --version=staging \
   ../dist-workspace/packages/api/app-staging.yaml
-gcloud --quiet app deploy \
-  --version=staging \
-  ../dist-workspace/packages/client/app-staging.yaml
-gcloud --quiet app deploy \
-  dispatch.yaml
+
+# gcloud --quiet app deploy \
+#   --version=staging \
+#   ../dist-workspace/packages/client/app-staging.yaml
+
+# gcloud --quiet app deploy \
+#   dispatch.yaml
